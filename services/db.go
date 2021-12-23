85007a2733b000742c06d24d6548806a63dc1321
coolwhite520 <81929154@qq.com>
2021-12-23 22:41:18 +0800

    使用mysql，防止sqlite3 lock

--- services/db.go
+++ services/db.go
@@ -2,56 +2,57 @@ package services
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 )
 
 var db *sql.DB
 
-const initSql = `
-CREATE TABLE IF NOT EXISTS "tbl_user" (
-   "Id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-   "Username"	TEXT NOT NULL UNIQUE,
-   "HashedPassword"	BLOB NOT NULL,
-   "IsSuper" TINYINT,
-   "CreatedAt" DATETIME DEFAULT (datetime('now','localtime'))
-);
+var SqlArr = []string {
+`CREATE DATABASE IF NOT EXISTS translate_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;`,
+`use translate_db;`,
+`CREATE TABLE IF NOT EXISTS tbl_user (
+   Id int(11) NOT NULL AUTO_INCREMENT,
+   Username	VARCHAR(255) UNIQUE,
+   HashedPassword	BLOB NOT NULL,
+   IsSuper TINYINT,
+   CreatedAt DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (Id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+`CREATE TABLE IF NOT EXISTS tbl_record (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Sha1	VARCHAR(255),
+  Content	TEXT,
+  ContentType TEXT,
+  TransType INTEGER,
+  OutputContent TEXT,
+  SrcLang TEXT,
+  DesLang TEXT,
+  FileName TEXT,
+  DirRandId TEXT,
+  State INTEGER,
+  StateDescribe TEXT,
+  Error TEXT,
+  UserId INTEGER,
+  CreateAt DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX(Sha1),
+  PRIMARY KEY (Id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+}
 
-CREATE TABLE IF NOT EXISTS "tbl_record" (
-  "Id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-  "Sha1"	TEXT,
-  "Content"	TEXT,
-  "ContentType" TEXT,
-  "TransType" INTEGER,
-  "OutputContent" TEXT,
-  "SrcLang" TEXT,
-  "DesLang" TEXT,
-  "FileName" TEXT,
-  "DirRandId" TEXT,
-  "State" INTEGER,
-  "StateDescribe" TEXT,
-  "Error" TEXT,
-  "UserId" INTEGER,
-  "CreateAt" DATETIME DEFAULT (datetime('now','localtime'))
-);
-CREATE INDEX IF NOT EXISTS tbl_record_sha1_idx ON tbl_record(Sha1);
-
-CREATE TABLE IF NOT EXISTS "tbl_component" (
-    "ImageName" TEXT,
-    "FileName" TEXT,
-    "FilePath" text,
-    "Version" TEXT,
-    "FileMd5" TEXT,
-    "CreateAt" DATETIME DEFAULT (datetime('now','localtime'))
-);
-`
 
 func init() {
-	var filename = "translate.db"
-	db, _ = sql.Open("sqlite3", filename)
-	_, err := db.Exec(initSql)
+	var err error
+	db, err = sql.Open("mysql", "root:112233@tcp(127.0.0.1:3306)/?charset=utf8&parseTime=True")
 	if err != nil {
 		log.Errorln(err)
 		return
+	}
+	for _,v := range SqlArr {
+		exec, err := db.Exec(v)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(exec)
 	}
 }
