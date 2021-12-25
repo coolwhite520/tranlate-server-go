d10c3310c7a7f44d93d5c6b79164aa2993a4bcfa
coolwhite520 <81929154@qq.com>
2021-12-25 15:38:21 +0800

    修改install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 yum install ./rpms/*.rpm
 wget https://copr.fedorainfracloud.org/coprs/ngompa/musl-libc/repo/epel-7/ngompa-musl-libc-epel-7.repo -O /etc/yum.repos.d/ngompa-musl-libc-epel-7.repo
 yum install -y musl-libc-static
+systemctl start docker
 chmod +x ./translate-server
 ./translate-server 
