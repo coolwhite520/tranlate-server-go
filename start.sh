4aaf212a100311bbb4e0c7375d2d3e8776b532d5
coolwhite520 <81929154@qq.com>
2022-01-04 14:40:58 +0800

    g

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-nohup ./translate-server & 
+#!/bin/bash
+# shellcheck disable=SC2009
+SYSTEM=$(uname -s)                    #获取操作系统类型
+if [ "$SYSTEM" = "Linux" ] ; then
+pid=$(ps -ef | grep dockerd | grep -v grep | awk -F" " '{print $2}')
+if [ ${#pid} -ge 0 ]; then
+    kill -9 "$pid"
+fi
+dockerd&
+nohup ./translate-server &
+fi
+
+
+
